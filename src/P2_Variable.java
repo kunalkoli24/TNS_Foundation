85c25da7e55259fc90a761a5a901716a5c8345c7
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-01-29 14:36:24 +0530

    Integer:23 Float:33.33 Long:2342344 String:kunal Boolean:true Char:K Double:2.23423424E8 Byte:100

---
+++ src/P2_Variable.java
@@ -0,0 +1,23 @@
+public class P2_Variable {
+    public static void main(String[] args) {
+        // To declare vairable
+        int k = 23;
+        float f = 33.33f;
+        long l = 2342344;
+        String s = "kunal";
+        boolean b = true;
+        char c = 'K';
+        double d = 223423424;
+        byte B = 'd';
+
+        System.out.println("Integer:" +k);
+        System.out.println("Float:" +f);
+        System.out.println("Long:" +l);
+        System.out.println("String:" +s);
+        System.out.println("Boolean:"+b);
+        System.out.println("Char:" +c);
+        System.out.println("Double:" +d);
+        System.out.println("Byte:" +B);
+
+    }
+}
