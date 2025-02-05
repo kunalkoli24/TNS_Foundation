790bf1bbe0f719ea28eefae28afee1e2e7d83d59
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-05 16:08:17 +0530

    Do While loop

---
+++ src/P9_Do_While_Loop.java
@@ -0,0 +1,11 @@
+public class P9_Do_While_Loop {
+    public static void main(String[] args) {
+        int i=90;
+
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while (i<=99);
+    }
+}
