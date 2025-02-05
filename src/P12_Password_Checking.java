f203a051004f38fa65cdd340169e5eb3309b9d34
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-05 21:43:17 +0530

    To Check Login Credentials

---
+++ src/P12_Password_Checking.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class P12_Password_Checking {
+    public static void main(String[] args) {
+        String userName = "kunal";
+        String password = "kunal@2409";
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter User Name: ");
+        String uName = input.nextLine();
+        System.out.print("Enter Your Password: ");
+        String pass = input.nextLine();
+
+        if(uName.equals(userName) && pass.equals(password)){
+            System.out.println("Welcome User");
+        } else if (uName.equals(userName) && !pass.equals(password)) {
+            System.out.println("Invalid Password");
+        } else if (!uName.equals(userName) && pass.equals(password)) {
+            System.out.println("Invalid User-Name");
+        }
+        else if (!uName.equals(userName) && !pass.equals(password)) {
+            System.out.println("Invalid Credentials");
+        }
+    }
+}
