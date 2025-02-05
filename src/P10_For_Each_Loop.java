f578b7194b3ae3ef0dbc885c8f4c9b057bca1fd7
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-05 16:08:44 +0530

    For Each loop

---
+++ src/P10_For_Each_Loop.java
@@ -0,0 +1,9 @@
+public class P10_For_Each_Loop {
+    public static void main(String[] args) {
+        int[] a={23,53,11,76,43,77,33};
+
+        for(int i: a){
+            System.out.println(i);
+        }
+    }
+}
