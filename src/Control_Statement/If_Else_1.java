e9229f9efe9cb4f88f99447e9df5ac7608aa1b09
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-05 13:51:13 +0530

    if_else

--- src/Control_Statement/If_Else_1.java
+++ src/Control_Statement/If_Else_1.java
@@ -4,7 +4,7 @@ public class If_Else_1 {
     public static void main(String[] args) {
         int Age = -11;
 
-        if (0<Age&&Age<18){
+        if (0<Age && Age<18){
             System.out.println("Your Are Minor");
         } else if (Age>=18) {
             System.out.println("Your Are Adult");
