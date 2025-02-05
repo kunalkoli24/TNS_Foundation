3e9480aaabb6a19b420e307c8d42ae4ba3a229c4
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-05 14:35:58 +0530

    while loop

---
+++ src/P8_While_loop.java
@@ -0,0 +1,10 @@
+public class P8_While_loop {
+    public static void main(String[] args) {
+        int i =10;
+
+        while(i<=100){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
