e793cd354362f8f36691acbd1e5f624de1c57f0d
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-05 14:20:26 +0530

    P

--- src/P1Program.java
+++ src/P1Program.java
@@ -1,5 +1,5 @@
 public class P1Program {
     public static void main(String[] args) {
-        System.out.println("Kunal love vaishnavi...");
+        System.out.println("Hello World...");
     }
 }
