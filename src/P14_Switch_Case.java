1702e746fba753481d8ced00ec1f3f41b21dd939
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-05 16:10:04 +0530

    Switch Case

---
+++ src/P14_Switch_Case.java
@@ -0,0 +1,12 @@
+public class P14_Switch_Case {
+    public static void main(String[] args) {
+        char x = 'p';
+        String lang = switch (x){
+          case 'a' -> "Angular";
+          case 't' -> "TailwindCss";
+          case 'p' -> "Python";
+            default -> "Wrong Choice";
+        };
+        System.out.println(lang);
+    }
+}
