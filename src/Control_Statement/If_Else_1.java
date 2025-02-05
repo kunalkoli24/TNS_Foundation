9724104f0c26a6af873c3202a5276e2e1e519761
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-05 13:55:00 +0530

    if_else

--- src/Control_Statement/If_Else_1.java
+++ src/Control_Statement/If_Else_1.java
@@ -5,9 +5,9 @@ public class If_Else_1 {
         int Age = -11;
 
         if (0<Age && Age<18){
-            System.out.println("Your Are Minor");
+            System.out.println("You Are Minor");
         } else if (Age>=18) {
-            System.out.println("Your Are Adult");
+            System.out.println("You Are Adult");
         }
         else {
             System.out.println("Invalid Age");
