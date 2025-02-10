564d02417259a60539a72f4ff688b23e1004ef52
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-10 14:42:45 +0530

    operator

---
+++ src/P6_incre_decre_Operator.java
@@ -0,0 +1,11 @@
+public class P6_incre_decre_Operator {
+    public static void main(String[] args) {
+        int a=9;
+        int b=10;
+        int c=a++ +a+ ++b;
+        System.out.println(c);
+        int d=a++ +c+ ++b;
+        System.out.println(d);
+    }
+
+}
