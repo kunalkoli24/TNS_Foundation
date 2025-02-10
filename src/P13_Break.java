4b09cad8515e67ca87874775b271645015daaa95
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-10 14:44:45 +0530

    Break

---
+++ src/P13_Break.java
@@ -0,0 +1,12 @@
+public class P13_Break {
+    public static void main(String[] args) {
+        int i=10;
+        for (int j = i; j <20 ; j++) {
+            if(j==15){
+                break;
+            }
+            System.out.println(j);
+    }
+
+}
+}
