1fb75c1d9a5d7a14124fb45cec8e613a60b3008b
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-11 13:59:19 +0530

    Getter Setter Testing

---
+++ src/P20GetterSetter/TestGetterSetter.java
@@ -0,0 +1,13 @@
+package P20GetterSetter;
+
+public class TestGetterSetter {
+    public static void main(String[] args) {
+        GetterSetter P1=new GetterSetter();
+        P1.setName("vaishnavi");
+        P1.setpCity("Pune");
+        System.out.println(P1);
+        GetterSetter P2=new GetterSetter("Kunal","Mumbai");
+        System.out.println(P2);
+}
+
+}
