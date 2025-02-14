ae17bc6eea416cedbf6f49d58ba5e31a0b644786
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-14 20:42:00 +0530

    Testing Inheritance person,citizen class

---
+++ src/P21Inheritance/TestingInheritance.java
@@ -0,0 +1,8 @@
+package P21Inheritance;
+
+public class TestingInheritance {
+    public static void main(String[] args) {
+        Student s1 = new Student("kunal",1234567890,"Pune",20,"Keystone");
+        System.out.println(s1);
+    }
+}
