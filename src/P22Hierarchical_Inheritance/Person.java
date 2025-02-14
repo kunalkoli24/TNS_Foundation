105366dc1eb78555dfa1742314befe4890a70797
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-14 20:43:43 +0530

    Inheritance person class

---
+++ src/P22Hierarchical_Inheritance/Person.java
@@ -0,0 +1,41 @@
+package P22Hierarchical_Inheritance;
+
+public class Person {
+    private String name ;
+    private String city;
+
+    public Person() {
+        System.out.println("Default constructor ....");
+        name="kunal";
+        city="Kharadi";
+    }
+
+    public Person(String city, String name) {
+        this.city = city;
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "city='" + city + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
