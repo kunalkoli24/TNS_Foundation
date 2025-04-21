be138574888f474546699aec0671889e27baf739
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-04-21 20:42:29 +0530

    Dice-miniproject

---
+++ src/P30Array/NewArr.java
@@ -0,0 +1,11 @@
+package P30Array;
+
+public class NewArr {
+    public static void main(String[] args) {
+        int newArr[] = {33,45,65,20,30,44};
+
+        for (int i=0;i<newArr.length;i++){
+            System.out.println(newArr[i]);
+        }
+    }
+}
